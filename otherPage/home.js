89e3b325c5a103ee8207e45e02f11462834abb12
red-one12 <radoanahmed1242000@gmail.com>
2024-09-19 10:52:44 +0600

    add money feature done

--- otherPage/home.js
+++ otherPage/home.js
@@ -1,2 +1,37 @@
+const addMoneyForm = document.getElementById("add-money-form");
+
+// document.getElementById('add-money-btn-up').addEventListener('click', function(){
+
+//   addMoneyForm.style.display = 'block'
+
+document
+  .getElementById("add-money-btn-down")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const balanceAmount = document.getElementById("balance-amount");
+    const amount = document.getElementById("amount");
+    const password = document.getElementById("password");
+
+    if (String(password.value) === "1111") {
+      let balanceAmountFinal = Number(balanceAmount.innerText);
+
+      balanceAmountFinal += Number(amount.value);
+
+      balanceAmount.innerText = balanceAmountFinal;
 
 
+      document.getElementById('successfully-added').innerText = 'Successfully Added...';
+      document.getElementById('wrong-password').innerText = '';
+      
+      amount.value = "";
+      password.value = "";
+    } else {
+      document.getElementById('successfully-added').innerText = '';
+      document.getElementById('wrong-password').innerText = 'Wrong Password';
+      amount.value = "";
+      password.value = "";
+    }
+  });
+
+// })
