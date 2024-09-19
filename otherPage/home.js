803bd4371294e431702bf1d10e75565ca0d0e21e
red-one12 <radoanahmed1242000@gmail.com>
2024-09-19 12:44:44 +0600

    all done

--- otherPage/home.js
+++ otherPage/home.js
@@ -1,37 +1,84 @@
 const addMoneyForm = document.getElementById("add-money-form");
+const cashOutForm = document.getElementById("cash-out-form");
 
-// document.getElementById('add-money-btn-up').addEventListener('click', function(){
+document.getElementById("add-money-btn-up").addEventListener("click", function () {
+  addMoneyForm.style.display = "block";
+  cashOutForm.style.display = "none";
 
-//   addMoneyForm.style.display = 'block'
+  document.getElementById("successfully-added-for-add").innerText = "";
+  document.getElementById("wrong-password-for-add").innerText = "";
+});
 
-document
-  .getElementById("add-money-btn-down")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
+document.getElementById("cash-out-btn-up").addEventListener("click", function () {
+  cashOutForm.style.display = "block";
+  addMoneyForm.style.display = "none";
 
-    const balanceAmount = document.getElementById("balance-amount");
-    const amount = document.getElementById("amount");
-    const password = document.getElementById("password");
+  document.getElementById("successfully-added-for-cash").innerText = "";
+  document.getElementById("wrong-password-for-cash").innerText = "";
+  document.getElementById("insufficient-for-cash").innerText = "";
+});
 
-    if (String(password.value) === "1111") {
-      let balanceAmountFinal = Number(balanceAmount.innerText);
+document.getElementById("add-money-btn-down").addEventListener("click", function (event) {
+  event.preventDefault();
 
-      balanceAmountFinal += Number(amount.value);
+  const balanceAmount = document.getElementById("balance-amount");
+  const amount = document.getElementById("amount");
+  const password = document.getElementById("password");
+
+  if (password.value === "1111") {
+    let balanceAmountFinal = Number(balanceAmount.innerText);
+    balanceAmountFinal += Number(amount.value);
+
+    balanceAmount.innerText = balanceAmountFinal;
+
+    document.getElementById("successfully-added-for-add").innerText = "Successfully Added...";
+    document.getElementById("wrong-password-for-add").innerText = "";
+
+    amount.value = "";
+    password.value = "";
+  } else {
+    document.getElementById("successfully-added-for-add").innerText = "";
+    document.getElementById("wrong-password-for-add").innerText = "Wrong Password";
+    amount.value = "";
+    password.value = "";
+  }
+});
+
+document.getElementById("cash-out-btn-down").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const balanceAmount = document.getElementById("balance-amount");
+  const amount = document.getElementById("balance-amount-cash-out");
+  const password = document.getElementById("password-cash-out");
+
+  let balanceAmountFinal = Number(balanceAmount.innerText);
+
+  if (Number(amount.value) <= balanceAmountFinal) {
+    if (password.value === "1111") {
+      balanceAmountFinal -= Number(amount.value);
 
       balanceAmount.innerText = balanceAmountFinal;
 
+      document.getElementById("successfully-added-for-cash").innerText = "Successfully Transferred";
+      document.getElementById("wrong-password-for-cash").innerText = "";
+      document.getElementById("insufficient-for-cash").innerText = "";
 
-      document.getElementById('successfully-added').innerText = 'Successfully Added...';
-      document.getElementById('wrong-password').innerText = '';
-      
       amount.value = "";
       password.value = "";
     } else {
-      document.getElementById('successfully-added').innerText = '';
-      document.getElementById('wrong-password').innerText = 'Wrong Password';
+      document.getElementById("successfully-added-for-cash").innerText = "";
+      document.getElementById("wrong-password-for-cash").innerText = "Wrong Password";
+      document.getElementById("insufficient-for-cash").innerText = "";
+
       amount.value = "";
       password.value = "";
     }
-  });
+  } else {
+    document.getElementById("successfully-added-for-cash").innerText = "";
+    document.getElementById("wrong-password-for-cash").innerText = "";
+    document.getElementById("insufficient-for-cash").innerText = "Insufficient Balance";
 
-// })
+    amount.value = "";
+    password.value = "";
+  }
+});
